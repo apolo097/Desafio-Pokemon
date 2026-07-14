4c4dbf30c0dbf332abb583ae021d218f4e172968
Matheus <matheusrocha.apple@gmail.com>
2026-07-14 17:32:00 -0300

    Criei uma função para buscar pokemon pelo nome

--- main.py
+++ main.py
@@ -1,41 +1,49 @@
-import requests
+def buscar_pokemon(nome):
 
-url = "https://pokeapi.co/api/v2/pokemon/charizard"
+    import requests
 
-resposta = requests.get(url)
-dados = resposta.json()
+    url = f"https://pokeapi.co/api/v2/pokemon/{nome}"
 
-tipos = []
-for item in dados["types"]:
-    tipos.append(item["type"]["name"])
-
-habilidades = []
-for item in dados["abilities"]:
-    habilidades.append(item["ability"]["name"])
-
-stats = {}
-for item in dados["stats"]:
-    nome_stat = item["stat"]["name"]
-    valor_stat = item["base_stat"]
-    stats[nome_stat] = valor_stat
-
-## print(stats) -> caso queira ver que aqui ele esta juntando em uma lista todos juntos. Como se eu criasse outro json de dados. Idem ao que vem da api por exemplo. ai temos dados e stats
-
-pokemon = {    
-    "nome": dados["name"],
-    "tipo(s)": ", ".join(tipos),
-    "hp": stats["hp"],
-    "attack":stats["attack"],
-    "defense":stats["defense"],
-    "special_attack":stats["special-attack"],
-    "special_defense":stats["special-defense"],
-    "speed":stats["speed"],
-    "altura":dados["height"]/10,
-    "peso":dados["weight"]/10, ##dividido por 10 para ficar em medidas humanas
-    "habilidade(s)": ", ".join(habilidades),
-    "total_stats":sum(stats.values())
-}
+    resposta = requests.get(url)
+    dados = resposta.json()
 
 
+
+    tipos = []
+    for item in dados["types"]:
+        tipos.append(item["type"]["name"])
+
+    habilidades = []
+    for item in dados["abilities"]:
+        habilidades.append(item["ability"]["name"])
+
+    stats = {}
+    for item in dados["stats"]:
+        nome_stat = item["stat"]["name"]
+        valor_stat = item["base_stat"]
+        stats[nome_stat] = valor_stat
+
+    ## print(stats) -> caso queira ver que aqui ele esta juntando em uma lista todos juntos. Como se eu criasse outro json de dados. Idem ao que vem da api por exemplo. ai temos dados e stats
+
+    pokemon = {    
+        "nome": dados["name"],
+        "tipo(s)": ", ".join(tipos),
+        "hp": stats["hp"],
+        "attack":stats["attack"],
+        "defense":stats["defense"],
+        "special_attack":stats["special-attack"],
+        "special_defense":stats["special-defense"],
+        "speed":stats["speed"],
+        "altura":dados["height"]/10,
+        "peso":dados["weight"]/10, ##dividido por 10 para ficar em medidas humanas
+        "habilidade(s)": ", ".join(habilidades),
+        "total_stats":sum(stats.values())
+    }
+
+
+
+    return pokemon
+
+pokemon = buscar_pokemon("torchic")
 print(pokemon)
 
